43c884bd1114c0d5029661f09784c7179da6da13
madhuri-cha <166789780+madhuri-cha@users.noreply.github.com>
2026-01-05 00:00:05 +0530

    Refactor API setup to use a production URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,14 @@
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: import.meta.env.VITE_API_URL,
+// });
+
+// export default api;
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+export const API = axios.create({
+  baseURL: "https://inventory-production-a60e.up.railway.app",
+  withCredentials: true
 });
 
-export default api;
